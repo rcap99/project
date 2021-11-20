328b3ca296d782caf4e6591532bafaabd72320a7
win10 <>
2021-11-20 19:32:22 +0100

    test commit

---
+++ ProjectSE/test/projectse/ComplexNumberTest.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package projectse;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class ComplexNumberTest {
+    
+    public ComplexNumberTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
