6c20358d9f76114dd94bd3a52827e92f020ef59c
win10 <>
2021-11-22 17:17:56 +0100

    TestFixedDupSwapCommit

--- ProjectSE/test/projectse/ComplexStackTest.java
+++ ProjectSE/test/projectse/ComplexStackTest.java
@@ -4,6 +4,7 @@
  */
 package projectse;
 
+import java.util.EmptyStackException;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -17,7 +18,7 @@ public class ComplexStackTest {
        cstack=new ComplexStack();
    }
     
-   @Test
+   @Test(expected=EmptyStackException.class)
    public void testDup(){
        ComplexNumber c1=new ComplexNumber(1.1,1.1);
        cstack.push(new ComplexNumber(1.1,1.1));
@@ -25,9 +26,21 @@ public class ComplexStackTest {
        assertEquals(c1, cstack.pop());
        assertEquals(c1, cstack.pop());
        assertTrue(cstack.isEmpty());
+       cstack.dup();
    }
    
-   
+   @Test(expected=EmptyStackException.class)
+   public void testSwap(){
+       ComplexNumber c1=new ComplexNumber(1.1,1.1);
+       ComplexNumber c2=new ComplexNumber(2.2,2.2);
+       cstack.push(new ComplexNumber(1.1,1.1));
+       cstack.push(new ComplexNumber(2.2,2.2));
+       cstack.swap();
+       assertEquals(c1, cstack.pop());
+       assertEquals(c2, cstack.pop());
+       assertTrue(cstack.isEmpty());
+       cstack.swap();
+   }
    
    @After 
    public void cleanUp(){
