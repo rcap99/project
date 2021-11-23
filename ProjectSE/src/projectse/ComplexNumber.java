a8cd9b0d8e73ecc085929eda6fe0a20c91e8dceb
PRINCIPAL <PRINCIPAL@192.168.56.1>
2021-11-23 20:30:57 +0100

    Sixth Commint

--- ProjectSE/src/projectse/ComplexNumber.java
+++ ProjectSE/src/projectse/ComplexNumber.java
@@ -39,8 +39,8 @@ public class ComplexNumber {
     public String toString() {
         if (im == 0) return re + "";
         if (re == 0) return im + "i";
-        if (im <  0) return re + " - " + (-im) + "i";
-        return re + " + " + im + "i";
+        if (im <  0) return re + "-" + (-im) + "i";
+        return re + "+" + im + "i";
     }
     
     //return modulus of a complex number
