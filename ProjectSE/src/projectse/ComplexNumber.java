8ed6331eaeb0f7bc8fffca30c973e1306d58aca0
win10 <>
2021-11-23 21:24:08 +0100

    fixCommit

--- ProjectSE/src/projectse/ComplexNumber.java
+++ ProjectSE/src/projectse/ComplexNumber.java
@@ -4,8 +4,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package projectse;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+
 
 /**
  *
