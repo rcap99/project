1c2bdabc676ef5de5542c8c5db7d81aebc723394
win10 <win10@DESKTOP-DO74OKP.mshome.net>
2021-11-25 09:45:10 +0100

    Revert "Added application icon"
    This reverts commit ca8e683c02ccdbc8f7a4e5b51d2d2724ae8b4256.

--- ProjectSE/src/projectse/ProjectSE.java
+++ ProjectSE/src/projectse/ProjectSE.java
@@ -8,7 +8,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -21,7 +20,6 @@ public class ProjectSE extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Calculator");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         stage.show();
     }
 
