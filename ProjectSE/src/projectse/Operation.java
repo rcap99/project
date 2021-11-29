7d7d53c71e261c3790581cbcfd638a9e2a6163fb
PRINCIPAL <PRINCIPAL@192.168.56.1>
2021-11-29 16:23:39 +0100

    Change Name

---
+++ ProjectSE/src/projectse/Operation.java
@@ -0,0 +1,16 @@
+package projectse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *Operation Interface
+ *Supports the execute and modify commands
+ * @author Group11
+ */
+public interface Operation {
+    void execute();
+    void modify();
+}
