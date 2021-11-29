bf87f0e1cd502c4c343ced50af9647394be2644b
PRINCIPAL <PRINCIPAL@192.168.56.1>
2021-11-29 15:59:15 +0100

    Operation Interface

---
+++ ProjectSE/src/projectse/OperationInterface.java
@@ -0,0 +1,15 @@
+package projectse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author PRINCIPAL
+ */
+public interface OperationInterface {
+    void execute();
+    void modify();
+}
