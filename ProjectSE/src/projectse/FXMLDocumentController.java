a6eea3239bdf94e876461d4e62c23b5314a1fc3e
win10 <>
2021-12-07 13:45:03 +0100

    fixrefactorAlertMethodCommit

--- ProjectSE/src/projectse/FXMLDocumentController.java
+++ ProjectSE/src/projectse/FXMLDocumentController.java
@@ -14,20 +14,10 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import projectse.alert.AlertClear;
-import projectse.alert.AlertReloadFile;
-import projectse.alert.AlertSaveFile;
-import projectse.alert.AlertStrategy;
+import projectse.alert.*;
 
 
 public class FXMLDocumentController implements Initializable {
@@ -58,8 +48,6 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void execute(ActionEvent event) {
         int returnValue=c.execute(textField.getText());
-        /*if(returnValue!=0)
-            c.alertError(returnValue,textField.getText());*/
         textField.clear();
         if(c.getComplexStack().hasChanged()){
             c.getComplexStack().notifyObservers();
