8f5c798fa0f8f2c6202a78991d79bc9b21cfc297
valer <valer@172.16.0.132>
2021-12-08 18:56:08 +0100

    Implemented test for execute method

--- ProjectSE/test/complexcalculator/CalculatorTest.java
+++ ProjectSE/test/complexcalculator/CalculatorTest.java
@@ -4,13 +4,13 @@
  */
 package complexcalculator;
 
-import complexcalculator.ComplexNumber;
-import complexcalculator.Calculator;
+import complexcalculator.stack.ComplexStack;
 import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class CalculatorTest {
-    Calculator c;
+    Calculator c1, c2;
     ComplexNumber n1;
     ComplexNumber n2;
     ComplexNumber n3;
@@ -19,35 +19,221 @@ public class CalculatorTest {
     
     @Before
     public void setUp(){
-        c = new Calculator();
+        c1 = new Calculator();
+        c2 = new Calculator();
         n1 = new ComplexNumber(1.0, 2.0);
         n2 = new ComplexNumber(-1.0, 2.0);
         n3 = new ComplexNumber(1.4, -2.0);
         n4 = new ComplexNumber(0, 2.0);
-        n5 = new ComplexNumber(1.0, 0);
+        n5 = new ComplexNumber(2.0, 0);
     }
     
-  /*  @Test
-    public void testGetLifoList(){
-        ComplexStack stack=c.getComplexStack();
-        
-        stack.push(n1);
-        stack.push(n2);
-        stack.push(n3);
-        stack.push(n4);
-        stack.push(n5);
+    @Test
+    public void testExecutePush(){
+        ComplexStack stack1=c1.getComplexStack();
+        stack1.push(n1);
+        stack1.push(n2);
+        stack1.push(n3);
+        stack1.push(n4);
+        stack1.push(n5);
+        c2.execute("1+2i");
+        c2.execute("-1+2i");
+        c2.execute("1.4 - 2i");
+        c2.execute("2i");
+        c2.execute("2");
+        assertEquals(stack1, c2.getComplexStack());
+    }
     
-        List<ComplexNumber> l=c.getLifoList();
-        assertEquals(n5,l.get(0));
-        assertEquals(n4,l.get(1));
-        assertEquals(n3,l.get(2));
-        assertEquals(n2,l.get(3));
-        assertEquals(n1,l.get(4));
-    }*/
+    @Test
+    public void testExecutBasicOperations(){
+        ComplexNumber a, b;
+        ComplexStack stack1=c1.getComplexStack();
+        stack1.push(n1);
+        stack1.push(n2);
+        b = stack1.pop();
+        a = stack1.pop();
+        stack1.push(a.add(b));
+        c2.execute("1+2i");
+        c2.execute("-1+2i");
+        c2.execute("+");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.pop();
+        c2.getComplexStack().pop();
+        stack1.push(n1);
+        stack1.push(n2);
+        b = stack1.pop();
+        a = stack1.pop();
+        stack1.push(a.subtract(b));
+        c2.execute("1+2i");
+        c2.execute("-1+2i");
+        c2.execute("-");
+        stack1.pop();
+        c2.getComplexStack().pop();
+        stack1.push(n1);
+        stack1.push(n2);
+        b = stack1.pop();
+        a = stack1.pop();
+        stack1.push(a.multiplication(b));
+        c2.execute("1+2i");
+        c2.execute("-1+2i");
+        c2.execute("*");
+        stack1.pop();
+        c2.getComplexStack().pop();
+        stack1.push(n1);
+        stack1.push(n2);
+        b = stack1.pop();
+        a = stack1.pop();
+        stack1.push(a.division(b));
+        c2.execute("1+2i");
+        c2.execute("-1+2i");
+        c2.execute("/");
+        stack1.pop();
+        c2.getComplexStack().pop();
+    }
+    
+    @Test
+    public void testExecuteStackOperations(){
+        ComplexStack stack1=c1.getComplexStack();
+        stack1.push(n1);
+        stack1.push(n2);
+        stack1.push(n3);
+        stack1.push(n4);
+        stack1.push(n5);
+        c2.execute("1+2i");
+        c2.execute("-1+2i");
+        c2.execute("1.4 - 2i");
+        c2.execute("2i");
+        c2.execute("2");
+        stack1.dup();
+        c2.execute("dup");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.drop();
+        c2.execute("drop");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.over();
+        c2.execute("over");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.swap();
+        c2.execute("swap");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.execute("clear");
+        assertEquals(stack1, c2.getComplexStack());
+    }
+    
+    @Test(expected=ExceptionInInitializerError.class)
+    public void testExecuteVariablesOperations(){
+        ComplexStack stack1=c1.getComplexStack();
+        stack1.push(n1);
+        c2.getComplexStack().push(n1);
+        c2.execute(">a");
+        c2.execute("<a");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1.add(n5));
+        c2.getComplexStack().push(n5);
+        c2.execute("+a");
+        c2.execute("<a");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        c2.getComplexStack().push(n5);
+        c2.execute("-a");
+        c2.execute("<a");
+        assertEquals(stack1, c2.getComplexStack());
+    }
+    
+    @Test
+    public void testExecuteTrascendentalOperations(){
+        ComplexStack stack1=c1.getComplexStack();
+        stack1.push(n1);
+        stack1.push(stack1.pop().sin());
+        c2.execute("1+2i");
+        c2.execute("sin");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().cos());
+        c2.execute("1+2i");
+        c2.execute("cos");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().tan());
+        c2.execute("1+2i");
+        c2.execute("tan");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().arcSin());
+        c2.execute("1+2i");
+        c2.execute("asin");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().arcCos());
+        c2.execute("1+2i");
+        c2.execute("acos");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().arcTan());
+        c2.execute("1+2i");
+        c2.execute("atan");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(new ComplexNumber(stack1.pop().abs(), 0));
+        c2.execute("1+2i");
+        c2.execute("mod");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(new ComplexNumber(stack1.pop().phase(), 0));
+        c2.execute("1+2i");
+        c2.execute("arg");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().logarithm());
+        c2.execute("1+2i");
+        c2.execute("log");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(stack1.pop().exp());
+        c2.execute("1+2i");
+        c2.execute("exp");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+        stack1.push(n1);
+        stack1.push(n5);
+        Double exp = stack1.pop().getRe();
+        stack1.push(stack1.pop().power(exp));
+        c2.execute("2");
+        c2.execute("1+2i");
+        c2.execute("pow");
+        assertEquals(stack1, c2.getComplexStack());
+        stack1.clear();
+        c2.getComplexStack().clear();
+    }
     
     @After
     public void cleanUp(){
-        c = null;
+        c1 = null;
+        c2 = null;
         n1 = null;
         n2 = null;
         n3 = null;
