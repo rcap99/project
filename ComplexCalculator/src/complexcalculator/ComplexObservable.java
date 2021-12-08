aff81e7ee66363faae72aa8e64011fa22925287d
valer <valer@172.16.0.132>
2021-12-08 09:43:29 +0100

    Renamed package name

---
+++ ComplexCalculator/src/complexcalculator/ComplexObservable.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package complexcalculator;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Concrete implementation of {@link ObservableInterface}.
+ * @author Group11
+ */
+public class ComplexObservable extends Observable implements ObservableInterface{
+
+    @Override
+    public void addObserver(Observer o) {
+        super.addObserver(o);
+    }
+
+    @Override
+    public void clearChanged() {
+        super.clearChanged();
+    }
+
+    @Override
+    public int countObservers() {
+        return super.countObservers();
+    }
+
+    @Override
+    public void deleteObserver(Observer o) {
+        super.deleteObserver(o);
+    }
+
+    @Override
+    public void deleteObservers() {
+        super.deleteObservers();
+    }
+
+    @Override
+    public boolean hasChanged() {
+        return super.hasChanged();
+    }
+
+    @Override
+    public void notifyObservers() {
+        super.notifyObservers();
+    }
+
+    @Override
+    public void notifyObservers(Object arg) {
+        super.notifyObservers(arg);
+    }
+
+    @Override
+    public void setChanged() {
+        super.setChanged();
+    }
+    
+    
+}
